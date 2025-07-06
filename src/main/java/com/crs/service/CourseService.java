ead53fe29b923d53d14fa293b90d2cfd1758641a
Navindi-Thisara <navindithisara214@gmail.com>
2025-07-05 22:41:13 -0300

    Create CourseService.java

--- src/main/java/com/crs/service/CourseService.java
+++ src/main/java/com/crs/service/CourseService.java
@@ -1,4 +1,48 @@
 package com.crs.service;
 
+import com.crs.model.Course;
+import com.crs.repository.CourseRepository;
+
+import java.util.List;
+
 public class CourseService {
+    private final CourseRepository courseRepo = new CourseRepository();
+
+    public boolean addCourse(Course course) {
+        try {
+            courseRepo.addCourse(course);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public List<Course> getAllCourses() {
+        return courseRepo.getAllCourses();
+    }
+
+    public Course getCourseById(int courseId) {
+        return courseRepo.getCourseById(courseId);
+    }
+
+    public boolean updateCourse(Course course) {
+        try {
+            courseRepo.updateCourse(course);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public boolean deleteCourse(int courseId) {
+        try {
+            courseRepo.deleteCourse(courseId);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
 }
